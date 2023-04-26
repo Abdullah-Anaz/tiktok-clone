4e274ca0f5f8cc8c73ed2474f37d6a13e935aa69
Abdullah Anaz <abdullahanaz@yahoo.com>
2023-04-27 00:41:06 +0530

    ADD: Firebase config

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyApv0vmrmkEUifefsrKuwcPBFkcWx6x8h0",
+  authDomain: "tiktok-clone-ca5ae.firebaseapp.com",
+  projectId: "tiktok-clone-ca5ae",
+  storageBucket: "tiktok-clone-ca5ae.appspot.com",
+  messagingSenderId: "283631237376",
+  appId: "1:283631237376:web:1ee6eaa7b5007728475be5",
+  measurementId: "G-HG6PZZM4JD",
+};
